d86b32b8b05390c8c6bc61f78995a3fdee75af2d
lucahardonk <43220862+lucahardonk@users.noreply.github.com>
2024-06-15 15:12:08 +0200

    Update memory_management.ino

--- memory_management/memory_management.ino
+++ memory_management/memory_management.ino
@@ -9,14 +9,43 @@ void setup() {
     lcd.backlight();
     lcd.clear();
     lcd.setCursor(0, 0);
+    lcd.print("Memory Test");
+    if(!memoryEqual(EEPROM1_ADDRESS,EEPROM2_ADDRESS)){
+      printEEPROM(EEPROM1_ADDRESS);
+      printEEPROM(EEPROM2_ADDRESS);
+      Serial.println("press 1 to copy EEPROM1_ADDRESS into  EEPROM2_ADDRESS and 2 for vice versa");
+      String input = "";
+      while (Serial.available() == 0) {// Do nothing, just wait}
+      // Read the input from the Serial Monitor
+      input = Serial.readString();
+      // Remove any newline characters from the input
+      input.trim();
+      
+      // Check if the input is a valid number
+      if(input.toInt() == 1 ){
+        copyAintoB(EEPROM1_ADDRESS, EEPROM2_ADDRESS);
+      }
+      else{
+        copyAintoB(EEPROM2_ADDRESS, EEPROM1_ADDRESS);
+      }
+    }
+  }
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
     lcd.print("EEPROM CLI Ready");
     Serial.begin(115200);
     delay(4000);
     Serial.println("EEPROM CLI Ready");
+    printNumberingCredential(EEPROM1_ADDRESS);
+    Serial.println("");
     Serial.println("Available commands:");
     Serial.println("1) erase - Erase all EEPROM memory");
     Serial.println("2) add <password> - Add a new password");
-    Serial.println("3) read - Read EEPROM contents");   
+    Serial.println("3) read - Read EEPROM contents"); 
+    Serial.println("4) delete - delete single object ");
+    Serial.println("5) print - print the credetnials ");
+    
 }
 
 void loop() {
@@ -115,9 +144,51 @@ void processCommand(String command) {
         }
         lcd.setCursor(0, 1);
         lcd.print("Read complete");
-    } else {
+    }else if(command == "delete"){
+      String input = "";  // Initialize an empty string to store user input
+      Serial.println("enter a number to delete: ");
+      // Wait for the user to enter input
+      while (Serial.available() == 0) {
+      // Do nothing, just wait
+        }
+
+      // Read the input from the Serial Monitor
+      input = Serial.readString();
+      // Remove any newline characters from the input
+      input.trim();
+      int input2=input.toInt();
+      // Check if the input is a valid number
+      if (isNumber(input)) {
+        Serial.print("You entered a valid number: ");
+        Serial.println(input2);
+      } else {
+        Serial.println("Invalid input!");
+      }
+      if(input2<=(numberPasswordSaved(EEPROM1_ADDRESS)-1)){
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Deleting...");
+        Serial.println("Erasing memory 1");
+        deleteCredentials(input2, EEPROM1_ADDRESS);
+        Serial.println("Erasing memory 2");
+        deleteCredentials(input2, EEPROM2_ADDRESS);
+        Serial.println("Complete!");
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("ready!");
+      }else{
+        Serial.println("Out of range!");
+      }
+    }else if(command == "print"){
+      Serial.println("---------mem 1 -------------");
+      printNumberingCredential(EEPROM1_ADDRESS);
+      Serial.println("---------mem 2 -------------");
+      printNumberingCredential(EEPROM2_ADDRESS);
+    }
+     else {
         lcd.setCursor(0, 0);
         lcd.print("Invalid command");
         Serial.println("Invalid command");
     }
 }
+
