d696e6cffccd0ad4a5c566af0dd0bfd86fad07c1
lucahardonk <43220862+lucahardonk@users.noreply.github.com>
2024-06-23 19:48:03 +0200

    added_joystick_and_buzzer_register_code

--- libraries/MyLibPasswordInjector/MyLibPasswordInjector.h
+++ libraries/MyLibPasswordInjector/MyLibPasswordInjector.h
@@ -11,7 +11,7 @@
 // Define the software serial pins
 #define RX_PIN 8
 #define TX_PIN 9
-#define BUZZERPIN 6
+#define BUZZER_PIN PD7
 
 struct Credential {
     char title[40];
@@ -28,6 +28,7 @@ extern int currentPasswordIndex;
 extern Credential currentPasswordInDisplay;
 extern bool refreshMenu;             
 extern int arrowPointing;
+extern volatile bool buzzerOn; // Declare buzzerOn as extern
 
 #define EEPROM1_ADDRESS 0x50
 #define EEPROM2_ADDRESS 0x51
@@ -36,10 +37,10 @@ extern int arrowPointing;
 #define CREDENTIAL_SIZE  140    // Page size of the EEPROM
 #define NUMBER_OF_PASSWORDS 234
 
-#define BUTTONPIN A2
-#define XAXISPIN A1
-#define YAXISPIN A0
-#define DEBOUNCE 200   //ms
+#define BUTTONPIN 5 //A2
+#define XAXISPIN 6 //A1
+#define YAXISPIN 7 //A0
+#define DEBAUNCE 200   //ms
 
 // Function Declarations
 void eraseEEPROM(int eepromAddress);
@@ -54,6 +55,7 @@ int findFirstFreeIndex(int eepromAddress);
 bool memoryEqual(int eepromAddress1, int eepromAddress2);
 int numberPasswordSaved(int EEPROM_ADDRESS);
 void readJoystick(int debaounceDelay);
+int analogReadChannel(uint8_t channel);
 void updateView(int arrow);
 void loadPasswords(int request_index, int max_passwords, Credential* saveCredentials);
 uint8_t getFingerprintID();
@@ -63,5 +65,6 @@ void deleteCredentials(int index, int EEPROM_ADDRESS);
 bool isNumber(String str);
 void copyAintoB(int eepromAddressA, int eepromAddressB);
 void printEEPROM(int eepromAddress);
+void setupBuzzer(int pin, int freq);
 
-#endif // PASSWORD_INJECTOR_UTILITY_FUNCTIONS_H
+#endif
